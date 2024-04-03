04af26e7c4262997b2f74113905fae04db1e38c8
Jonez de Mello Toffani da Silva <jmts39@hotmail.com>
2024-04-03 11:49:57 -0300

    ok

---
+++ 03-php_conexao_mysql/aula_015/tratar_original.php
@@ -0,0 +1,17 @@
+<?php
+
+// importar a class Database e configurações
+
+use sys4soft\Database;
+
+require_once('Database.php');
+
+// instanciação de objeto Database
+
+$connection = new Database()
+
+// parametros
+
+// inserir os dados do cliente
+
+// voltar ao formulário automaticamente
